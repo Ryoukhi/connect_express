a8473827658112c7ccfa5af2b66c0934f14bfb55
Krieger <stephen.deutou@gmail.com>
2025-12-05 05:02:18 +0100

    initialisation du backend

---
+++ connect-backend/src/test/java/com/eadl/connect_backend/ConnectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eadl.connect_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
