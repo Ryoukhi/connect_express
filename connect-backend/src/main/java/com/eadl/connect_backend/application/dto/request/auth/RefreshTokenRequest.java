2cd5a7371f04af8d17edf32bdaae5a713ae67e3f
Krieger <stephen.deutou@gmail.com>
2025-12-17 04:04:44 +0100

    add dto request/auth

--- connect-backend/src/main/java/com/eadl/connect_backend/application/dto/request/auth/RefreshTokenRequest.java
+++ connect-backend/src/main/java/com/eadl/connect_backend/application/dto/request/auth/RefreshTokenRequest.java
@@ -1,5 +1,28 @@
 package com.eadl.connect_backend.application.dto.request.auth;
 
-public class RefreshTokenRequest {
+import jakarta.validation.constraints.NotBlank;
 
+/**
+ * DTO pour la requête de rafraîchissement de token
+ */
+public class RefreshTokenRequest {
+    
+    @NotBlank(message = "Le refresh token est obligatoire")
+    private String refreshToken;
+    
+    // Constructeurs
+    public RefreshTokenRequest() {}
+    
+    public RefreshTokenRequest(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+    
+    // Getters & Setters
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+    
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
 }
